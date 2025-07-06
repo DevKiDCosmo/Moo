e2930d6e33ccdb816a0b537176edb25602f89f99
DevKiD <75792299+DevKiDCosmo@users.noreply.github.com>
2025-07-06 12:16:44 +0200

    Update export.h to improve visibility handling and add logging for non-Windows platforms

--- src/export.h
+++ src/export.h
@@ -1,11 +1,13 @@
 #pragma once
+#include <iostream>
 
 #if defined(_WIN32)
-  #ifdef MOOLIB_EXPORTS
+#ifdef MOOLIB_EXPORTS
     #define MOOLIB_API __declspec(dllexport)
-  #else
-    #define MOOLIB_API __declspec(dllimport)
-  #endif
+#else
+#define MOOLIB_API __declspec(dllexport)
+#endif
 #else
   #define MOOLIB_API __attribute__((visibility("default")))
+std::cout << "Building for non-Windows platform, using default visibility." << std::endl;
 #endif
