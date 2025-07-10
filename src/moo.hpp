f92b008d1bf8ac294b1dc9f2066e839665b5c2fd
DevKiD <75792299+DevKiDCosmo@users.noreply.github.com>
2025-07-10 18:46:49 +0200

    Adding now trigonometry.cpp - forgot to add moo.hpp back.

---
+++ src/moo.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <cstdint>
+#include <functional>
+#include <string>
+#include <variant>
+#include <vector>
+#include <optional>
+#include <condition_variable>
+#include <mutex>
+
+class moo {
+public:
+    struct FuncCall {
+        int64_t                 id;
+        std::string             name;
+        std::vector<int64_t>    params;
+        std::optional<double>   result;
+        std::condition_variable cv;
+        bool                    done = false;
+        std::mutex              mtx;
+    };
+
+    static double  absolute(double x);
+    static double  min(double a, double b);
+    static double  max(double a, double b);
+    static int64_t gcd(int64_t a, int64_t b);
+    static int64_t lcm(int64_t a, int64_t b);
+    static int64_t fac(int n);
+    static int64_t binom(int64_t n, int64_t k);
+    static double  flmod(double a, double b);
+    static int64_t mod(int64_t a, int64_t b);
+
+    static double  floor(double x);
+    static double  ceil(double x);
+    static int64_t idiv(double n, double k);
+
+    static int64_t* factable(int n);
+    static void     clearptr(const int64_t* ptr);
+    static int64_t* genPerm(int n);
+    static void     freeptr(int64_t* ptr);
+    static int64_t  permutation(int n, int k);
+    static int64_t  combination(int n, int k);
+    static double   pow(double base, int64_t exp);
+    static double   fpow(double base, double exp);
+    static double   round(double x);
+    static double   roundk(double x, int k);
+    static double   sqrt(double x);
+    static double   ksqrt(double x, double k);
+
+    static double ln(double x);
+    static double log(double x, double base = 10.0);
+    static double exp(double x);
+};
